c9489b8baa243c4d8d20901001c6945305cdfd9a
Balint Peter <potty0@gmail.com>
2017-02-20 16:52:41 +0100

    Added framework target

---
+++ iphone/ZBar/ZBar.h
@@ -0,0 +1,19 @@
+//
+//  ZBar.h
+//  ZBar
+//
+//  Created by Balint Peter on 2017. 02. 20..
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZBar.
+FOUNDATION_EXPORT double ZBarVersionNumber;
+
+//! Project version string for ZBar.
+FOUNDATION_EXPORT const unsigned char ZBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZBar/PublicHeader.h>
+
+
