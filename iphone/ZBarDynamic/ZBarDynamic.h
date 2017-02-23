e0333586e68e42f504ac59f0d16e9d817bf9f29e
Balint Peter <potty0@gmail.com>
2017-02-23 17:17:06 +0100

    project target update

---
+++ iphone/ZBarDynamic/ZBarDynamic.h
@@ -0,0 +1,19 @@
+//
+//  ZBarDynamic.h
+//  ZBarDynamic
+//
+//  Created by Balint Peter on 2017. 02. 23..
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZBarDynamic.
+FOUNDATION_EXPORT double ZBarDynamicVersionNumber;
+
+//! Project version string for ZBarDynamic.
+FOUNDATION_EXPORT const unsigned char ZBarDynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZBarDynamic/PublicHeader.h>
+
+
